83c8e35ad6084851dbaa8bd779a489f2e163549e
Nhan Le <nhanlego1@gmail.com>
2017-05-01 16:03:10 +0700

    update

--- sites/all/modules/custom/tour/themes/tour-admin-dashboard.tpl.php
+++ sites/all/modules/custom/tour/themes/tour-admin-dashboard.tpl.php
@@ -7,63 +7,64 @@
 ?>
 <link type="text/css" rel="stylesheet" href="<?php print base_path() . drupal_get_path('module', 'tour') ?>/css/admin_panel/admin-panel.css" media="all">
 <div id="admin-panel">
-    <h2>Manage Content</h2>
+    <h2><?php print t('Manage Content')?></h2>
     <ul>
-        <li class="manage"><a href="/admin/tours?destination=admin/dashboard">Manage tours</a></li>
-        <li class="manage"><a href="/admin/hosts?destination=admin/dashboard">Manage hosts</a></li>
-        <li class="manage"><a href="/admin/content/comment?destination=admin/dashboard">Manage comment</a></li>
-        <li class="manage"><a href="/admin/sliders?destination=admin/dashboard">Manage slider home</a></li>
-        <li class="manage"><a href="/admin/banner?destination=admin/dashboard">Manage Banner</a></li>
-        <li class="manage"><a href="/admin/blog?destination=admin/dashboard">Manage Blog</a></li>
-        <li class="manage"><a href="/admin/news?destination=admin/dashboard">Manage News</a></li>
-        <li class="manage"><a href="/admin/events?destination=admin/dashboard">Manage Events</a></li>
-        <li class="manage"><a href="/admin/pages?destination=admin/dashboard">Manage Static Pages</a></li>
-        <li class="manage"><a href="/admin/links?destination=admin/dashboard">Manage Links block</a></li>
-        <li class="manage"><a href="/admin/orders?destination=admin/dashboard">Manage Orders</a></li>
-        <li class="manage"><a href="/admin/question?destination=admin/dashboard">Manage Question</a></li>
-        <li class="manage"><a href="/admin/answers?destination=admin/dashboard">Manage Answers</a></li>
-        <li class="manage"><a href="/admin/gallery?destination=admin/dashboard">Manage Gallery</a></li>
-        <li class="manage"><a href="/admin/services?destination=admin/dashboard">Manage Services</a></li>
-        <li class="manage"><a href="/admin/testimonial?destination=admin/dashboard">Manage Testimonial</a></li>
-        <li class="manage"><a href="/admin/community?destination=admin/dashboard">Manage Community</a></li>
-        <li class="manage"><a href="/admin/structure/taxonomy/region?destination=admin/dashboard">Manage Prefecture </a></li>
-        <li class="manage"><a href="/admin/structure/taxonomy/theme_and_category?destination=admin/dashboard">Manage Theme and Category</a></li>
-        <li class="manage"><a href="/admin/structure/taxonomy/language?destination=admin/dashboard">Manage Support Languages</a></li>
-        <li class="manage"><a href="/admin/structure/taxonomy/blog?destination=admin/dashboard">Manage Blog Archive</a></li>
-        <li class="manage"><a href="/admin/structure/taxonomy/gallery?destination=admin/dashboard">Manage Gallery Category</a></li>
+        <li class="manage"><a href="/admin/tours?destination=admin/dashboard"><?php print t('Manage tours')?></a></li>
+        <li class="manage"><a href="/admin/hosts?destination=admin/dashboard"><?php print t('Manage hosts')?></a></li>
+        <li class="manage"><a href="/admin/content/comment?destination=admin/dashboard"><?php print t('Manage comment')?></a></li>
+        <li class="manage"><a href="/admin/sliders?destination=admin/dashboard"><?php print t('Manage slider home')?></a></li>
+        <li class="manage"><a href="/admin/banner?destination=admin/dashboard"><?php print t('Manage Banner')?></a></li>
+        <li class="manage"><a href="/admin/blog?destination=admin/dashboard"><?php print t('Manage Blog')?></a></li>
+        <li class="manage"><a href="/admin/news?destination=admin/dashboard"><?php print t('Manage News')?></a></li>
+        <li class="manage"><a href="/admin/events?destination=admin/dashboard"><?php print t('Manage Events')?></a></li>
+        <li class="manage"><a href="/admin/company?destination=admin/dashboard"><?php print t('Manage Company profile')?></a></li>
+        <li class="manage"><a href="/admin/pages?destination=admin/dashboard"><?php print t('Manage Static Pages')?></a></li>
+        <li class="manage"><a href="/admin/links?destination=admin/dashboard"><?php print t('Manage Links block')?></a></li>
+        <li class="manage"><a href="/admin/orders?destination=admin/dashboard"><?php print t('Manage Orders')?></a></li>
+        <li class="manage"><a href="/admin/question?destination=admin/dashboard"><?php print t('Manage Question')?></a></li>
+        <li class="manage"><a href="/admin/answers?destination=admin/dashboard"><?php print t('Manage Answers')?></a></li>
+        <li class="manage"><a href="/admin/gallery?destination=admin/dashboard"><?php print t('Manage Gallery')?></a></li>
+        <li class="manage"><a href="/admin/services?destination=admin/dashboard"><?php print t('Manage Services')?></a></li>
+        <li class="manage"><a href="/admin/testimonial?destination=admin/dashboard"><?php print t('Manage Testimonial')?></a></li>
+        <li class="manage"><a href="/admin/community?destination=admin/dashboard"><?php print t('Manage Community')?></a></li>
+        <li class="manage"><a href="/admin/structure/taxonomy/region?destination=admin/dashboard"><?php print t('Manage Prefecture')?> </a></li>
+        <li class="manage"><a href="/admin/structure/taxonomy/theme_and_category?destination=admin/dashboard"><?php print t('Manage Theme and Category')?></a></li>
+        <li class="manage"><a href="/admin/structure/taxonomy/language?destination=admin/dashboard"><?php print t('Manage Support Languages')?></a></li>
+        <li class="manage"><a href="/admin/structure/taxonomy/blog?destination=admin/dashboard"><?php print t('Manage Blog Archive')?></a></li>
+        <li class="manage"><a href="/admin/structure/taxonomy/gallery?destination=admin/dashboard"><?php print t('Manage Gallery Category')?></a></li>
     </ul>
 </div>
 <div id="admin-panel">
 
-    <h2>Manage Block Content</h2>
+    <h2><?php print t('Manage Block Content')?></h2>
     <ul>
-        <li class="manage"><a href="/admin/structure/block/manage/block/3/configure?destination=admin/dashboard">Manage block about us footer</a></li>
-        <li class="manage"><a href="/admin/structure/block/manage/block/2/configure?destination=admin/dashboard">Manage Block About us home</a></li>
-        <li class="manage"><a href="/admin/structure/block/manage/block/4/configure?destination=admin/dashboard">Manage block text add host</a></li>
-        <li class="manage"><a href="/admin/structure/block/manage/block/5/configure?destination=admin/dashboard">Manage block text add tour</a></li>
-        <li class="manage"><a href="/admin/structure/block/manage/block/6/configure?destination=admin/dashboard">Manage block text finish host</a></li>
-        <li class="manage"><a href="/admin/structure/block/manage/block/7/configure?destination=admin/dashboard">Manage block text finish tour</a></li>
+        <li class="manage"><a href="/admin/structure/block/manage/block/3/configure?destination=admin/dashboard"><?php print t('Manage block about us footer')?></a></li>
+        <li class="manage"><a href="/admin/structure/block/manage/block/2/configure?destination=admin/dashboard"><?php print t('Manage Block About us home')?></a></li>
+        <li class="manage"><a href="/admin/structure/block/manage/block/4/configure?destination=admin/dashboard"><?php print t('Manage block text add host')?></a></li>
+        <li class="manage"><a href="/admin/structure/block/manage/block/5/configure?destination=admin/dashboard"><?php print t('Manage block text add tour')?></a></li>
+        <li class="manage"><a href="/admin/structure/block/manage/block/6/configure?destination=admin/dashboard"><?php print t('Manage block text finish host')?></a></li>
+        <li class="manage"><a href="/admin/structure/block/manage/block/7/configure?destination=admin/dashboard"><?php print t('Manage block text finish tour')?></a></li>
     </ul>
 </div>
 <div id="admin-panel">
 
-    <h2>Additional Content</h2>
+    <h2><?php print t('Additional Content')?></h2>
     <ul>
-        <li class="add"><a href="/node/add/host?destination=admin/dashboard">Add Host</a></li>
-        <li class="add"><a href="/node/add/tour?destination=admin/dashboard">Add Tour</a></li>
-        <li class="add"><a href="/node/add/page?destination=admin/dashboard">Add Pages</a></li>
-        <li class="add"><a href="/node/add/blog?destination=admin/dashboard">Add Blog</a></li>
-        <li class="add"><a href="/node/add/events?destination=admin/dashboard">Add Events</a></li>
-        <li class="add"><a href="/node/add/news?destination=admin/dashboard">Add News</a></li>
-        <li class="add"><a href="/node/add/slider?destination=admin/dashboard">Add Slider</a></li>
-        <li class="add"><a href="/node/add/banner?destination=admin/dashboard">Add Banner</a></li>
-        <li class="add"><a href="/node/add/gallery?destination=admin/dashboard">Add Gallery</a></li>
-        <li class="add"><a href="/node/add/service?destination=admin/dashboard">Add Service</a></li>
-        <li class="add"><a href="/node/add/testimonial?destination=admin/dashboard">Add Testimonial</a></li>
-        <li class="add"><a href="/node/add/links?destination=admin/dashboard">Add Links</a></li>
-        <li class="add"><a href="/node/add/question?destination=admin/dashboard">Add Question</a></li>
-        <li class="add"><a href="/node/add/answer?destination=admin/dashboard">Add Answer</a></li>
-        <li class="add"><a href="/node/add/community?destination=admin/dashboard">Add Community</a></li>
+        <li class="add"><a href="/node/add/host?destination=admin/dashboard"><?php print t('Add Host')?></a></li>
+        <li class="add"><a href="/node/add/tour?destination=admin/dashboard"><?php print t('Add Tour')?></a></li>
+        <li class="add"><a href="/node/add/page?destination=admin/dashboard"><?php print t('Add Pages')?></a></li>
+        <li class="add"><a href="/node/add/blog?destination=admin/dashboard"><?php print t('Add Blog')?></a></li>
+        <li class="add"><a href="/node/add/events?destination=admin/dashboard"><?php print t('Add Events')?></a></li>
+        <li class="add"><a href="/node/add/news?destination=admin/dashboard"><?php print t('Add News')?></a></li>
+        <li class="add"><a href="/node/add/slider?destination=admin/dashboard"><?php print t('Add Slider')?></a></li>
+        <li class="add"><a href="/node/add/banner?destination=admin/dashboard"><?php print t('Add Banner')?></a></li>
+        <li class="add"><a href="/node/add/gallery?destination=admin/dashboard"><?php print t('Add Gallery')?></a></li>
+        <li class="add"><a href="/node/add/service?destination=admin/dashboard"><?php print t('Add Service')?></a></li>
+        <li class="add"><a href="/node/add/testimonial?destination=admin/dashboard"><?php print t('Add Testimonial')?></a></li>
+        <li class="add"><a href="/node/add/links?destination=admin/dashboard"><?php print t('Add Links')?></a></li>
+        <li class="add"><a href="/node/add/question?destination=admin/dashboard"><?php print t('Add Question')?></a></li>
+        <li class="add"><a href="/node/add/answer?destination=admin/dashboard"><?php print t('Add Answer')?></a></li>
+        <li class="add"><a href="/node/add/community?destination=admin/dashboard"><?php print t('Add Community')?></a></li>
      
     </ul>
 </div>
