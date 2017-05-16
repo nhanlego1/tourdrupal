43cd1e1bca8b56b0ccb0814e6ee64d7fa01cc3d6
splasher <splasher@Splash-aaa.local>
2017-05-16 16:45:32 +0700

    update

---
+++ sites/all/modules/custom/tour/themes/tour-contact-form.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$form = $element;
+?>
+<h2><?php print t('Contact') ?></h2>
+<table>
+    <tr>
+        <th><?php print t('Your name'); ?> <em class="required">※</em></th>
+        <td><?php print render($form['name']) ?></td>
+    </tr>
+    <tr>
+        <th><?php print t('Your Email'); ?> <em class="required">※</em></th>
+        <td><?php print render($form['email']) ?></td>
+    </tr>
+    <tr>
+        <th><?php print t('Subject'); ?> <em class="required">※</em></th>
+        <td><?php print render($form['subject']) ?></td>
+    </tr>
+    <tr>
+        <th><?php print t('Message'); ?> <em class="required">※</em></th>
+        <td><?php print render($form['message']) ?></td>
+    </tr>
+    
+
+</table>
+
+<?php print drupal_render_children($form) ?> 
