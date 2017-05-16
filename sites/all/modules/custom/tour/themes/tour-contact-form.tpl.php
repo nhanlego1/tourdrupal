e19ef709632437d8fbc0696a0a932e9c14f88d6b
splasher <splasher@Splash-aaa.local>
2017-05-16 17:02:45 +0700

    update

--- sites/all/modules/custom/tour/themes/tour-contact-form.tpl.php
+++ sites/all/modules/custom/tour/themes/tour-contact-form.tpl.php
@@ -38,6 +38,8 @@ global $language;
     
 
 </table>
+<div class="contact-content-footer">
+  <?php print drupal_render_children($form) ?> 
+</div>
 
-<?php print drupal_render_children($form) ?> 
-<p></p>
+
