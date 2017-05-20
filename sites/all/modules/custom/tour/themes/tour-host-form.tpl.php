12b6117c4d79cbe446b8c157f9d9ed583c07ec1b
splasher <splasher@Splash-aaa.local>
2017-05-20 22:48:21 +0700

    update

--- sites/all/modules/custom/tour/themes/tour-host-form.tpl.php
+++ sites/all/modules/custom/tour/themes/tour-host-form.tpl.php
@@ -84,7 +84,7 @@ $form = $element;
         <td>
             <div class="form-child-city">
                 <div class="prefecture">
-                    <span><?php print t('Prefectures') ?></span>
+                    <span><font><?php print t('Area') ?> ① </font><?php print t('Prefectures') ?></span>
                     <?php print render($form['field_guidearea1']) ?>
                 </div>
                 <div class="prefecture">
@@ -94,7 +94,7 @@ $form = $element;
             </div>
             <div class="form-child-city">
                 <div class="prefecture">
-                    <span><?php print t('Prefectures') ?></span>
+                    <span><font><?php print t('Area') ?> ② </font><?php print t('Prefectures') ?></span>
                     <?php print render($form['field_guidearea2']) ?>
                 </div>
                 <div class="prefecture">
@@ -104,7 +104,7 @@ $form = $element;
             </div>
             <div class="form-child-city">
                 <div class="prefecture">
-                    <span><?php print t('Prefectures') ?></span>
+                    <span><font><?php print t('Area') ?> ③ </font><?php print t('Prefectures') ?></span>
                     <?php print render($form['field_guidearea3']) ?>
                 </div>
                 <div class="prefecture">
@@ -114,7 +114,7 @@ $form = $element;
             </div>
             <div class="form-child-city">
                 <div class="prefecture">
-                    <span><?php print t('Prefectures') ?></span>
+                    <span><font><?php print t('Area') ?> ④ </font><?php print t('Prefectures') ?></span>
                     <?php print render($form['field_guidearea4']) ?>
                 </div>
                 <div class="prefecture">
